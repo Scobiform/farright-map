70f64043f3a5122321a0592f0eed22e026ec0673
Scobiform <datenkrake@scobiform.com>
2024-09-06 09:52:34 +0200

    switch to OpenStreetMap.DE

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
             {({ TileLayer, Marker, Popup }) => (
               <>
                 <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  url="https://tile.openstreetmap.de/{z}/{x}/{y}.png'"
                   attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
 
