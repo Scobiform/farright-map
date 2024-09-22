75724ee4ef048ad9e2165f47c1bb46d98f8963e3
Scobiform <datenkrake@scobiform.com>
2024-09-22 16:06:21 +0200

    kreis data update

--- src/components/Map/DynamicMap.js
+++ src/components/Map/DynamicMap.js
@@ -58,7 +58,7 @@ const DynamicMap = ({ polygons = [], ToolTip = "", children, className, width = 
 
       {/* Render Polygons */}
       {polygons.map((polygonCoords, index) => (
-        <Polygon key={index} positions={polygonCoords} color="rgba(140,210,140,0.0)" />
+        <Polygon key={index} positions={polygonCoords} color="rgba(210,210,140,0.0)" />
       ))}
 
       {/* Render additional children passed into the Map component */}
