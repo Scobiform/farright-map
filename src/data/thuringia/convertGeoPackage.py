8c8091cae7d8c643d4217f9505e97fad4bb7db5d
Scobiform <datenkrake@scobiform.com>
2024-10-09 19:53:57 +0200

    Import Thüringen Landtagswahl 2024 AfD Listen- und Direktbewerber #15

--- src/data/thuringia/convertGeoPackage.py
+++ src/data/thuringia/convertGeoPackage.py
@@ -1,3 +1,11 @@
+# Convert GeoPackage to GeoJSON
+# Source: Thüringer Landesamt für Statistik
+# 16TH_L24_Wahlkreiseinteilung.gpkg
+# https://wahlen.thueringen.de/landtagswahlen/lw_informationen.asp
+# © GDI-Th / Thüringer Landesamt für Statistik
+# Lizenz: Datenlizenz Deutschland – Namensnennung – Version 2.0
+# https://www.govdata.de/dl-de/by-2-0
+
 import geopandas as gpd
 import json
 from pyproj import CRS, Transformer
